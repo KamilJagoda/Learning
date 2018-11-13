a3532bff037fc977d3333e01a4de91a65a10bee7
KamilJagoda <kamil.jagoda@gate-software.com>
2018-11-13 09:16:05 +0100

    ES6 Arrow Function

---
+++ ES6-Basics/arrow-function.js
@@ -0,0 +1,9 @@
+const greetingOne = function (name) {
+    console.log("Hello, " + name);
+};
+
+const greetingTwo = name => console.log("Hello there, " + name);
+
+
+greetingOne("Kamil");
+greetingTwo("Kamil");
