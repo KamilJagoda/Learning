024cb9dd150bab19d6bdc6796e6ce6a425426104
KamilJagoda <kamil.jagoda@gate-software.com>
2018-11-14 10:42:35 +0100

    Vue Routes Basics

---
+++ Vue-Routes-Basics/src/components/Navbar.vue
@@ -0,0 +1,63 @@
+<template>
+  <nav class="navbar">
+    <ul>
+      <li><router-link :to="{ name: 'Home' }">Home</router-link></li>
+      <li><router-link :to="{ name: 'About' }">About</router-link></li>
+    </ul>
+
+    <h2>Users Profiles</h2>
+    <ul>
+      <li v-for="(id, index) in userIds" :key="index">
+        <router-link :to="{ name: 'ViewProfile', params: { user_id: id }}">
+          <span>Profile {{ id }}</span>
+        </router-link>
+      </li>
+    </ul>
+
+    <h2>Navigation Controls</h2>
+    <ul>
+      <li><button @click="goBack">Go Back</button></li>
+      <li><button @click="goHome">Redirect to home</button></li>
+      <li><button @click="goForward">Go Forward</button></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: 'Navbar',
+    data () {
+      return {
+        userIds: ['1', '2', '3','4']
+      }
+    },
+    methods: {
+      goBack() {
+        this.$router.go(-1)
+      },
+      goForward() {
+        this.$router.go(1 )
+      },
+      goHome() {
+        this.$router.push({ name: 'Home',})
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  
+  ul {
+    padding: 0;
+  }
+  
+  a {
+    color: #42b983;
+  }
+  
+  li {
+    list-style-type: none;
+    display: inline-block;
+    margin: 10px;
+  }
+</style>
